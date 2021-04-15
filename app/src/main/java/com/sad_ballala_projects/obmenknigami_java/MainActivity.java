c683a30ff509ace71b6025964e87900dd2154df3
ilya <ilya.balashow1@gmail.com>
2021-04-15 17:09:12 +0600

    Registration

--- app/src/main/java/com/sad_ballala_projects/obmenknigami_java/MainActivity.java
+++ app/src/main/java/com/sad_ballala_projects/obmenknigami_java/MainActivity.java
@@ -1,11 +1,16 @@
 package com.sad_ballala_projects.obmenknigami_java;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
@@ -132,6 +137,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
             case R.id.id_sign_up:
+                signUpDialog(R.string.ac_sign_up, R.string.sign_up_action);
 
                 break;
 
@@ -144,5 +150,26 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
         }
         return true;
+    }
+
+    private void signUpDialog(int title, int buttonTitle){
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.sign_up_layout, null);
+        dialogBuilder.setView(dialogView);
+        TextView titleTextView = dialogView.findViewById(R.id.tvAlertTitle);
+        titleTextView.setText(title);
+        Button b = dialogView.findViewById(R.id.buttonSignUp);
+        b.setText(buttonTitle);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
+
+
     }
 }
