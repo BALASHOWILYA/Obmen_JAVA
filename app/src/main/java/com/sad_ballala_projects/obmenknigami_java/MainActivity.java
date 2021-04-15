126c15de82b65b79e53e1b92bf80d83eed53a0a2
ilya <ilya.balashow1@gmail.com>
2021-04-15 13:45:49 +0600

    myCodeisnotDone

---
+++ app/src/main/java/com/sad_ballala_projects/obmenknigami_java/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sad_ballala_projects.obmenknigami_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
