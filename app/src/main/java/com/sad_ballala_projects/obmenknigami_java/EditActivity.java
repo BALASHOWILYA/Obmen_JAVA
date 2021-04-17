b166bd3857698ac01c28787af8aa8b9accf60a0b
ilya <ilya.balashow1@gmail.com>
2021-04-17 21:45:08 +0600

    addSpinner

--- app/src/main/java/com/sad_ballala_projects/obmenknigami_java/EditActivity.java
+++ app/src/main/java/com/sad_ballala_projects/obmenknigami_java/EditActivity.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -24,15 +25,16 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicMarkableReference;
 
 public class EditActivity extends AppCompatActivity {
     // Create a Cloud Storage reference from the app
-
+    private Spinner spinner;
     private StorageReference mStorageRef;
     private ImageView imItem;
     private Uri uploadUri;
-    
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,8 +46,11 @@ public class EditActivity extends AppCompatActivity {
 
     private void init(){
 
-
-
+        spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.category_spinner, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
         mStorageRef = FirebaseStorage.getInstance().getReference("Images");
         imItem = findViewById(R.id.imItem);
     }
