529a8bf6741bded2c411b8d1e645bf1f5dac3e1f
ilya <ilya.balashow1@gmail.com>
2021-04-17 17:05:21 +0600

    getImageIsDone

--- app/src/main/java/com/sad_ballala_projects/obmenknigami_java/EditActivity.java
+++ app/src/main/java/com/sad_ballala_projects/obmenknigami_java/EditActivity.java
@@ -3,20 +3,39 @@ package com.sad_ballala_projects.obmenknigami_java;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EditActivity extends AppCompatActivity {
+    private ImageView imItem;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_layout);
+        init();
     }
-    
 
-    public void onClickImage(View View){
 
+    private void init(){
+        imItem = findViewById(R.id.imItem);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 10 && data != null && data.getData() != null){
+            if(resultCode == RESULT_OK){
+                imItem.setImageURI(data.getData());
+            }
+        }
+    }
+
+    public void OnClickImage(View View){
+        getImage();
     }
 
     private void getImage(){
