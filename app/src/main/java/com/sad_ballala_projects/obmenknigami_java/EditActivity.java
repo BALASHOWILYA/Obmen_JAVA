fd687e1fd1f1c1abefe41081dde66112ae631886
ilya <ilya.balashow1@gmail.com>
2021-04-17 16:36:17 +0600

    getImage

--- app/src/main/java/com/sad_ballala_projects/obmenknigami_java/EditActivity.java
+++ app/src/main/java/com/sad_ballala_projects/obmenknigami_java/EditActivity.java
@@ -1,6 +1,8 @@
 package com.sad_ballala_projects.obmenknigami_java;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,4 +13,17 @@ public class EditActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_layout);
     }
+    
+
+    public void onClickImage(View View){
+
+    }
+
+    private void getImage(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 10);
+    }
+
 }
