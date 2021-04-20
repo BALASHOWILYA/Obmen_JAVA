56c392fa5546ad30914fe12bec615f720c9ad8f2
ilya <ilya.balashow1@gmail.com>
2021-04-20 20:01:53 +0600

    allInit

--- app/src/main/java/com/sad_ballala_projects/obmenknigami_java/ShowLayoutActivity.java
+++ app/src/main/java/com/sad_ballala_projects/obmenknigami_java/ShowLayoutActivity.java
@@ -2,18 +2,27 @@ package com.sad_ballala_projects.obmenknigami_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.sad_ballala_projects.obmenknigami_java.utils.MyConstants;
+import com.squareup.picasso.Picasso;
+
 
 public class ShowLayoutActivity extends AppCompatActivity {
     private TextView tvTitle, tvExchange, tvDisc, tvTel;
     private ImageView imMain;
 
+    public ShowLayoutActivity() {
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_layout);
+        init();
     }
 
     private void init(){
@@ -21,5 +30,16 @@ public class ShowLayoutActivity extends AppCompatActivity {
         tvTel = findViewById(R.id.tvMainTel);
         tvExchange = findViewById(R.id.tvMainExchange);
         tvDisc = findViewById(R.id.tvMainDisc);
+        imMain = findViewById(R.id.imMain);
+        if(getIntent() != null){
+            Intent i = getIntent();
+            tvTitle.setText(i.getStringExtra(MyConstants.TITLE));
+            tvTel.setText(i.getStringExtra(MyConstants.TEL));
+            tvExchange.setText(i.getStringExtra(MyConstants.CHANGE));
+            tvDisc.setText(i.getStringExtra(MyConstants.DISC));
+            Picasso.get().load(i.getStringExtra(MyConstants.IMAGE_ID)).into(imMain);
+
+        }
+
     }
 }
